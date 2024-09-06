5f4bd7dc8fbb0e1f8334f45c18167e1b236b0d35
robithhammada <156164108+tormented666@users.noreply.github.com>
2024-09-06 08:04:30 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,75 +6,205 @@
     <title>Nigga</title>
 </head>
 <body>
+
+    <h1>Nigg's</h1>
     <?php 
-    $nama = "six";
-    $nama2 = "Nigg's";
-    $angka = 1000;
-    $angka2 = 6000;
-    $angka2--;
-    $tambah = $angka2 / $angka;
-    $text = "Yang Anjing Masuk Kandang";
-    $kandang = array('Kucing', 'Anjing', 'Babi');
-    $owner = ['Oliver', 'Alissa', 'Sriyanto'];
-    $data = array( "Nama"   => "Oliver",
-                   "Umur"   => 38,
-                   "Negara" => "United Kingdom"
-                );
-    $data2 = array( "Istri"   => "Alissa",
-                    "Mobil"   => "Tesla"                
-                );
-    $data['Nama'] = "Oliver Sykes";
-    $data3 = array(
-        array("Wachid", "500", "Sangat Malas"),
-        array("Sugeng", "1000", "Malas Sedang"),
-        array("agoY", "400", "Sedikit Malas")
-    );
+    // $nama = "six";
+    // $nama2 = "Nigg's";
 
-    echo "<h1>$nama2</h1>";
-    echo "$nama";
-    echo "<br>";
-    echo $angka + $angka2;
-    echo "<br>";
-    echo $angka + $angka2 * $angka2;
-    echo "<br>";
-    echo $tambah;
-    echo "<br>";
-    echo "Angka terhitam adalah ". rand(1, 1000000);
-    echo "<br>";
-    echo "Angka terhitam adalah ". min($angka, $angka2, $tambah);
-    echo "<br>";
-    echo str_replace("Anjing", "Babi", $text);
-    echo "<br>";
-    echo str_repeat(str_replace("Anjing", "Babi", $text), 5);
-    echo "<br>";
-    print_r($kandang);
-    echo "<br>";
-    echo $kandang[0]; 
-    echo "<br>";
-    echo $kandang[2];
-    echo "<br>";
-    echo $owner[0];
-    echo "<br>";
-    shuffle($kandang);
-    print_r($kandang);
-    echo "<br>";
-    sort($owner);
-    print_r($owner);
-    echo "<br>";
-    echo "Nama orang itu adalah " . $data['Nama'];
-    echo "<br>";
-    echo "Umurnya " . $data['Umur'] . " Tahun";
-    echo "<br>";
-    echo "Dia dari Negara " . $data['Negara'];
-    echo "<br>";
-    print_r( array_merge($data, $data2));
-    print_r($data3);
-    echo "<br>";
-    echo $data3[0][2];
-    echo "<br>";
-    $data3[2][2] = "Pemalas Sekali";
-    echo $data3[2][2]
+    // $data = array( "Nama"   => "Oliver",
+    //                "Umur"   => 38,
+    //                "Negara" => "United Kingdom"
+    //             );
+    // $data2 = array( "Istri"   => "Alissa",
+    //                 "Mobil"   => "Tesla"                
+    //             );
+    // $data['Nama'] = "Oliver Sykes";
+    // $data3 = array(
+    //     array("Wachid", "500", "Sangat Malas"),
+    //     array("Sugeng", "1000", "Malas Sedang"),
+    //     array("agoY", "400", "Sedikit Malas")
+    // );
 
+    // echo "<h1>$nama2</h1>";
+    // echo "<br>";
+    // print_r($kandang);
+    // echo "<br>";
+    // echo $kandang[0]; 
+    // echo "<br>";
+    // echo $kandang[2];
+    // echo "<br>";
+    // echo $owner[0];
+    // echo "<br>";
+    // shuffle($kandang);
+    // print_r($kandang);
+    // echo "<br>";
+    // sort($owner);
+    // print_r($owner);
+    // echo "<br>";
+    // echo "Nama orang itu adalah " . $data['Nama'];
+    // echo "<br>";
+    // echo "Umurnya " . $data['Umur'] . " Tahun";
+    // echo "<br>";
+    // echo "Dia dari Negara " . $data['Negara'];
+    // echo "<br>";
+    // print_r( array_merge($data, $data2));
+    // print_r($data3);
+    // echo "<br>";
+    // echo $data3[0][2];
+    // echo "<br>";
+    // $data3[2][2] = "Pemalas Sekali";
+    // echo $data3[2][2];
+    // echo "<br>";
+
+    // for($i=0; $i<count($kandang); $i++){
+    //     echo $kandang[$i];
+    //     echo "<br>";
+    // }
+
+    // foreach($kandang as $k){
+    //     echo $k;
+    //     echo "<br>";
+    // }
+
+    // $data = ['nama' => 'Wachid',
+    //          'umur' => '500',
+    //          'sifat' => 'Pemalas'];
+
+    // foreach($data as $key => $value){
+    //     echo $value . "<br>";
+    // }
+
+    // $hewan = ['Anjing', 'Kucing', 'Babi', 'Cicak'];
+    // $i = 0;
+
+    // while( $i < count($hewan)){
+    //     echo $hewan[$i];
+    //     $i++;
+    // }
+
+    // do{
+    //     echo $hewan[$i]. "<br>";
+    //     $i++;
+    // }while( $i < count($hewan));
+   
+
+    // $password = '1234';
+    // $password2 = 1234;
+    
+    // if( $password == $password2 ){
+    //     echo ' anda berhasil masuk!';
+    // }else{
+    //     echo ' gagal! password salah';
+    // }
+
+    // $uang_programmer = 4000;
+    // $keyboard        = 3000;
+    // $uang_designer   = 4000;
+
+    // if( $uang_programmer > $keyboard
+    //     && $uang_designer > $keyboard){
+
+    //         echo 'boleh beli keyboard';
+    //     }else{
+    //         echo 'ngga bisa beli';
+    //     }
+
+    // if( $uang_programmer > $keyboard){
+    //     echo 'dibeli!';
+    // }else if( $uang_designer > $keyboard){
+    //     echo 'dibeli oleh designer!';
+
+    //     if($uang_designer >= $keyboard * 2){
+    //         echo 'dibeli duakali oleh designer!';
+    //     }
+    // }else{
+    //     echo 'ngga cukup';
+    // }
+
+    // $nama = "Wach1d";
+
+    // switch ($nama) {
+    //     case 'agoY':
+    //         echo 'namanya agoY';
+    //         break;
+    //     case 'Sugeng':
+    //         echo 'namanya Sugeng';
+    //         break;
+    //     case 'Wachid':
+    //         echo 'namanya Wachid';
+    //         break;
+    //     default:
+    //     echo 'tidak ada yang cocok';
+    // }
+
+    // function print_text($text)
+    // {
+    //     $text = $text . "pulang ";
+    //     echo "----------";
+    //     echo "$text";
+    //     echo "----------";
+    // }
+
+    // function jarak()
+    // {
+    //     echo "<br>";
+    // }
+
+    // print_text("Wachid ");
+    // jarak();
+    // jarak();
+    // print_text("agoY ");
+    // jarak();
+    // jarak();
+    
+    // function menghitung($x, $y)
+    // {
+    //     $z = $x / $y;
+    //     return $z;
+    // }
+
+    // $hasil = menghitung(25, 50) * 1000;
+    // echo "hasilnya ". $hasil;
+
+    // $a = 200;
+    // $b = 500;
+
+    // function menghitung(){
+    //     global $a, $b;
+    //     $c = $a * $b;
+    //     return $c;
+    // }
+
+    // echo menghitung();
+
+    // $ngetik = function($text){
+    //     echo $text;
+    // };
+
+    // $greeting = $ngetik;
+
+    // $greeting( 'selamat datang semuanya 123 ');
+
+    // function berteriak($callback){
+    //     echo 'HALOOOO <br>';
+
+    //     if ( is_callable($callback)){
+    //         call_user_func($callback, 'selamat datang');
+    //     }else{
+    //         echo ' dia bukan fungsi';
+    //     }
+    // }
+
+    // $panggil = function($text){
+    //     echo $text;
+    // };
+
+    // berteriak( $panggil );
+
+    $kendaraan = [ 'mobil', 'motor'];
+    var_dump($kendaraan);
+    
 
 
 
